44389fd412ff509fbf5c3f42326300dc8573ddb3
MadHatter01 <r.vaishnavi15@gmail.com>
2020-02-02 14:06:35 -0800

    Added Plane Detection and Object Placement

---
+++ Assets/Scripts/ObjectPlacement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+
+public class ObjectPlacement : MonoBehaviour
+{
+    public GameObject cardObject;
+    private GameObject spawner;
+
+    private ARRaycastManager ar_RaycastManager;
+    private Vector2 touchPosition;
+
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+
+   private void Awake()
+    {
+        ar_RaycastManager = GetComponent<ARRaycastManager>();
+
+    }
+
+    bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+        if(Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+        touchPosition = default;
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!TryGetTouchPosition(out Vector2 touchPosition))
+        {
+            return;
+        }
+        if(ar_RaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
+        {
+            var hitPose = hits[0].pose;
+
+            if(spawner == null)
+            {
+                spawner = Instantiate(cardObject, hitPose.position, hitPose.rotation);
+            }
+            else
+            {
+                spawner.transform.position = hitPose.position;
+            }
+        }
+        
+    }
+}
