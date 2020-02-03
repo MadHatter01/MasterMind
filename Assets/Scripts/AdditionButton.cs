ec1bee365bf060c78bbf7e046123708a0bbb40bb
Sanlador <cunardr@oregonstate.edu>
2020-02-02 17:05:45 -0800

    Added concept spawning

---
+++ Assets/Scripts/AdditionButton.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdditionButton : MonoBehaviour
+{
+    public GameObject self, concept, lineH, lineV;
+    public int position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void setPosition(int p)
+    {
+        position = p;
+    }
+
+    void OnMouseDown()
+    {
+        if (3 == position)
+        {
+            Debug.Log("Pressed right");
+            Instantiate(concept, self.transform.position + new Vector3(2, 0, 0), Quaternion.identity);
+            Instantiate(lineH, self.transform.position + new Vector3(.6f, 0, 0), Quaternion.identity);
+        }
+        else if (position == 2)
+        {
+            Debug.Log("Pressed left");
+            Instantiate(concept, self.transform.position + new Vector3(-2, 0, 0), Quaternion.identity);
+            Instantiate(lineH, self.transform.position + new Vector3(-.6f, 0, 0), Quaternion.identity);
+        }
+        else
+        {
+            Debug.Log("Pressed top");
+            Instantiate(concept, self.transform.position + new Vector3(0, 2, 0), Quaternion.identity);
+            Instantiate(lineV, self.transform.position + new Vector3(0, .6f, 0), Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/Concept.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Concept : MonoBehaviour
+{
+    public GameObject button1, button2, button3, self;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject topButton = Instantiate(button1, self.transform.position + self.transform.up * .8f, self.transform.rotation) as GameObject;
+        AdditionButton top = topButton.GetComponent<AdditionButton>();
+        top.setPosition(1);
+        topButton.transform.Rotate(90, 0, 0);
+
+        GameObject leftButton = Instantiate(button1, self.transform.position + self.transform.right * .8f, self.transform.rotation) as GameObject;
+        AdditionButton left = leftButton.GetComponent<AdditionButton>();
+        left.setPosition(3);
+        leftButton.transform.Rotate(90, 0, 0);
+
+        GameObject rightButton = Instantiate(button1, self.transform.position + self.transform.right * -.8f, self.transform.rotation) as GameObject;
+        AdditionButton right = rightButton.GetComponent<AdditionButton>();
+        right.setPosition(2);
+        rightButton.transform.Rotate(90, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
